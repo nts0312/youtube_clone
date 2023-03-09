12949ae714ab512ec8f8d5b9aa8488686abd0999
nitinsinghm111 <nitinsinghm111@gmail.com>
2023-03-09 14:04:24 +0530

    first commit with api calling

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import { AppContext } from "./context/contextApi";
+
+const App = () => {
+  return <AppContext></AppContext>;
+};
+
+export default App;
