e51743c61371f83fe3abbfd3749e2739223a511d
nitinsinghm111 <nitinsinghm111@gmail.com>
2023-03-14 10:34:35 +0530

    Header for youtube clone added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
 import React from "react";
+//refce for basic react component template
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+import Header from "./components/Header";
+import Feed from "./components/Feed";
+import VideoDetails from "./components/VideoDetails";
+import SearchResult from "./components/SearchResult";
 import { AppContext } from "./context/contextApi";
 
 const App = () => {
-  return <AppContext></AppContext>;
+  return (
+    <AppContext>
+      <BrowserRouter>
+        <div className="flex flex-col h-full">
+          <Header />
+          <Routes>
+            <Route path="/" element={<Feed />} />
+            <Route
+              path="/searchResult/:searchQuery"
+              element={<SearchResult />}
+            />
+            <Route path="/video/:id" element={<VideoDetails />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </AppContext>
+  );
 };
 
 export default App;
