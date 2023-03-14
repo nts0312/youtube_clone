3a611b83a419b2d09e641ad81b0f702e8af3fc98
nitinsinghm111 <nitinsinghm111@gmail.com>
2023-03-14 16:44:07 +0530

    video cards showed on screen

--- src/components/VideoCard.js
+++ src/components/VideoCard.js
@@ -1,7 +1,19 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
-function VideoCard() {
-  return <div>VideoCard</div>;
+function VideoCard({ video }) {
+  return (
+    <Link to={`/video/${video?.videoId}`}>
+      <div className="flex flex-col mb-8">
+        <div className="relative h-48 md:h-40 md:rounded-xl overflow-hidden">
+          <img
+            className="h-full w-full object-cover"
+            src={video?.thumbnails?.[0]?.url}
+          />
+        </div>
+      </div>
+    </Link>
+  );
 }
 
 export default VideoCard;
